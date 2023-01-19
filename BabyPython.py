e2fcc66fa485548cd8f39cc89177faf31e21b354
Sabrina <121908417+SOwens-Create@users.noreply.github.com>
2023-01-18 22:46:18 -0500

    Add files via upload

---
+++ BabyPython.py
@@ -0,0 +1,29 @@
+
+#Start Preliminary Exercise
+#Create a set of numbers and save the number at index 1 as a new variable
+
+varset = [1, 2, 3, 4];
+var = varset[1];
+print(var);
+
+#print the variable at index -2, counting from the right of the list
+print(varset[-2]);
+
+#print 2 variables, starting from index 0
+print(varset[0:2]);
+
+#Create a new set of numbers, save in variable
+#print 3 variables, starting from index 1
+varset2 = [2, 5, 4, 7];
+print(varset2[1:3]);
+
+#End Preliminary Exercise
+
+
+
+
+
+
+
+
+
